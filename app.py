f542c2d1ce51611fae872ec95853e99c334ac3e6
Selia Rena <yanbo920223@gmail.com>
2026-04-17 07:56:48 +0800

    Feat(application): Add clockheart engine as backend workflow entry point :P

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.chronica.application.engine.test_engine import TestEngine
+from src.chronica.application.engine.clockheart_engine import ClockheartEngine
 import time
 import json
 import logging
@@ -15,19 +16,17 @@ TICK_DURATION_S = 1
 
 def main():
     configure_logging()
-    logger.info("----- Starting Chronica Test Engine -----")
-    engine = TestEngine()
-    engine.run()
+    logger.info("----- Starting Chronica Clockheart Engine Execution -----")
+    engine = ClockheartEngine()
+    engine.start()
     nowtime = 0
     while nowtime < TEST_DURATION_SECONDS:
         time.sleep(TICK_DURATION_S)
         engine.tick()
         nowtime += TICK_DURATION_S
     engine.stop()
-    logger.info("----- App Usage Report -----")
-    logger.info(json.dumps(engine.report.to_debug_dict(), indent=4, ensure_ascii=False))
-    logger.info("----- Session History -----")
-    logger.info(json.dumps(engine.history.to_debug_list(), indent=4, ensure_ascii=False))
+    logger.info("----- Chronica Clockheart Engine Execution Finished -----")
+    logger.info(engine.debug_dump)
 
 if __name__ == "__main__":
     main()
---
+++ src/chronica/application/engine/clockheart_engine.py
@@ -0,0 +1,93 @@
+from src.chronica.core.foreground_context_sampler import ForegroundContextSampler, SamplerResultStatus
+from src.chronica.core.sample_stream_sessionizer import SampleStreamSessionizer, SessionizerEvent, SessionizerResultStatus
+from src.chronica.domain.app_usage_report import AppUsageReport
+from src.chronica.domain.session_history import SessionHistory
+from src.chronica.domain.chronosystem import CascadedChronoSpan, CascadingType
+from src.chronica.common.formatters import DIGITAL_CLOCK
+from src.chronica.utils.json_util import to_pretty_json
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ClockheartEngine:
+    def __init__(self):
+        self._sampler = ForegroundContextSampler()
+        self._sessionizer = SampleStreamSessionizer()
+        self.report = AppUsageReport()
+        self.history = SessionHistory()
+    
+    @property
+    def current_cycle_duration(self) -> str:
+        duration = self.history.latest.end_ts_ms - self.history.oldest.start_ts_ms
+        return DIGITAL_CLOCK[CascadedChronoSpan.from_total_ms(duration).transform(CascadingType.FULL_PADDED)]
+    
+    @property
+    def debug_dump(self) -> str:
+        return to_pretty_json({
+            "App usage report of this cycle": self.report.to_debug_dict(),
+            "Full session history of this cycle": self.history.to_debug_list()
+        })
+
+    def start(self):
+        logger.info("Starting Clockheart Engine")
+        
+        sampler_result = self._sampler.start_sampling()
+        if sampler_result.status != SamplerResultStatus.SUCCESS:
+            logger.error("Failed to start sampler, returned status: [%s], message: %s", sampler_result.status.name, sampler_result.message)
+            return
+        else:
+            logger.info("Sampler started successfully, emitted event: %s", sampler_result.event.name)
+        logger.debug("Initial Sampler result: %s", to_pretty_json(sampler_result.to_debug_dict()))
+        
+        sessionizer_result = self._sessionizer.consume(sampler_result)
+        if sessionizer_result.status == SessionizerResultStatus.OP_FAILED:
+            logger.error("Sessionizer failed during initial consume: %s", sessionizer_result.message)
+            logger.error("Events emitted during failure: %s", [event.name for event in sessionizer_result.events])
+            logger.error("Error type: %s", sessionizer_result.error_type.name)
+        else:
+            logger.info("Sessionizer initialized successfully, emitted events: %s", [event.name for event in sessionizer_result.events])
+        logger.debug("Initial Sessionizer result: %s", to_pretty_json(sessionizer_result.to_debug_dict()))
+        
+    def stop(self):
+        logger.info("Stopping Clockheart Engine")
+        
+        sampler_result = self._sampler.stop_sampling()
+        if sampler_result.status != SamplerResultStatus.SUCCESS:
+            logger.error("Failed to stop sampler, returned status: [%s], message: %s", sampler_result.status.name, sampler_result.message)
+            return
+        else:
+            logger.info("Sampler stopped successfully, emitted event: %s", sampler_result.event.name)
+        logger.debug("Terminal Sampler result: %s", to_pretty_json(sampler_result.to_debug_dict()))
+        
+        sessionizer_result = self._sessionizer.consume(sampler_result)
+        if sessionizer_result.status == SessionizerResultStatus.OP_FAILED:
+            logger.error("Sessionizer failed during terminal consume: %s", sessionizer_result.message)
+            logger.error("Events emitted during failure: %s", [event.name for event in sessionizer_result.events])
+            logger.error("Error type: %s", sessionizer_result.error_type.name)
+        else:
+            logger.info("Sessionizer processed terminal sampler result successfully, emitted events: %s", [event.name for event in sessionizer_result.events])
+            self.report.add_session(sessionizer_result.session)
+            self.history.append(sessionizer_result.session)
+        logger.debug("Terminal Sessionizer result: %s", to_pretty_json(sessionizer_result.to_debug_dict()))
+        
+        logger.info("This cycle's duration: %s", self.current_cycle_duration)
+        
+    def tick(self):
+        logger.info("Ticking Clockheart Engine")
+        
+        sampler_result = self._sampler.on_tick()
+        sessionizer_result = self._sessionizer.consume(sampler_result)
+        
+        if SessionizerEvent.SESSION_EMITTED in sessionizer_result.events and sessionizer_result.status == SessionizerResultStatus.OP_SUCCESS:
+            logger.info("New session emitted during tick, adding to report and history")
+            self.report.add_session(sessionizer_result.session)
+            self.history.append(sessionizer_result.session)
+        elif sessionizer_result.status == SessionizerResultStatus.OP_FAILED:
+            logger.error("Sessionizer failed during tick consume: %s", sessionizer_result.message)
+            logger.error("Events emitted during failure: %s", [event.name for event in sessionizer_result.events])
+            logger.error("Error type: %s", sessionizer_result.error_type.name)
+        elif sessionizer_result.status == SessionizerResultStatus.OP_IGNORED:
+            logger.info("Same windows, no action taken.")
+            
+        if sessionizer_result.status != SessionizerResultStatus.OP_IGNORED:
+            logger.debug("Successful or error tick Sessionizer result: %s", to_pretty_json(sessionizer_result.to_debug_dict()))
